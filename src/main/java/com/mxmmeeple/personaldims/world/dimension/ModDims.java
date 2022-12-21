ca25b94d3da946b1611586f7b1bdb6b166d0e72b
Meeple <6298377+mxmeeple@users.noreply.github.com>
2022-12-21 05:35:34 +0000

    Fixed typo's

--- src/main/java/com/mxmmeeple/personaldims/world/dimension/ModDims.java
+++ src/main/java/com/mxmmeeple/personaldims/world/dimension/ModDims.java
@@ -14,9 +14,9 @@ import net.minecraft.world.level.dimension.DimensionType;
 
 public class ModDims {
 
-	public static final ResourceLocation PDIM_ID = new ResourceLocation(PersonalDims.MODID, "pdims");
+	public static final ResourceLocation PDIM_ID = new ResourceLocation(PersonalDims.MODID, "pdim");
     public static final ResourceKey<Level> PDIM_WORLD = ResourceKey.create(Registry.DIMENSION_REGISTRY, PDIM_ID);
-    public static final ResourceKey<DimensionType> PDIM_TYPE = ResourceKey.create(Registry.DIMENSION_TYPE_REGISTRY, PDIM_WORLD.getRegistryName());
+    public static final ResourceKey<DimensionType> PDIM_TYPE = ResourceKey.create(Registry.DIMENSION_TYPE_REGISTRY, PDIM_ID);
 	//private static final ResourceKey<LevelStem> POCKET_DIMENSION = ResourceKey.create(Registry.LEVEL_STEM_REGISTRY, PDIM_ID);
    
     private static final Logger LOGGER = LogUtils.getLogger();
